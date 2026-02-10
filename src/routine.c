0c0d7fdcb35693e445330753b57e7936d606e8f9
Kaide Lucas Da Silva <kaidda-s@c1r6p4.42sp.org.br>
2026-02-10 19:14:12 -0300

    Include print status

--- src/routine.c
+++ src/routine.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kaidda-s <kaidda-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: kaidda-s <kaidda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 22:18:12 by kaidda-s          #+#    #+#             */
-/*   Updated: 2026/02/09 22:50:35 by kaidda-s         ###   ########.fr       */
+/*   Updated: 2026/02/10 19:10:56 by kaidda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,19 @@ void	*philo_routine(void *arg)
 {
 	
 }
+
+void	print_status(t_philo *philo, char *message)
+{
+	long	timestamp;
+
+	pthread_mutex_lock(&philo->data->print_mutex);
+	if (philo->data->someone_died == 1)
+	{
+		pthread_mutex_unlock(philo->print_mutex);
+		return ;
+	}
+	timestamp = get_time() - philo->data->start_time;
+	printf("%ld %d %s\n", timestamp, philo->id, message);
+	pthread_mutex_unlock(philo->print_mutex);
+}
+
