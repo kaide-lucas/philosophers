206dc80292677aeb093ff0c538173456a6c932dc
Kaide <kaidejeronimo35@gmail.com>
2026-02-09 22:52:44 -0300

    Includefunction  time

--- src/time.c
+++ src/time.c
@@ -3,10 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   time.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kaidda-s <kaidda-s@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kaidda-s <kaidda-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 22:19:06 by kaidda-s          #+#    #+#             */
-/*   Updated: 2026/02/03 19:11:36 by kaidda-s         ###   ########.fr       */
+/*   Updated: 2026/02/09 00:43:12 by kaidda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philosophers.h"
+
+long	get_time(void)
+{
+	struct timeval tv;
+
+	if (gettimeofday(&tv, NULL))
+		return (0);
+	return ((tv.tv_sec * (long)1000) + (tv.tv_usec / 1000));
+}
+
+void	ft_usleep(long sleep_time)
+{
+	long	time;
+	
+	time = get_time();
+	while ((get_time() - time) < sleep_time)
+		usleep(500); 
+}
